a88d6aa22944cd486c5a58ffbd0741ee34ff4ed7
Heather Tweedie <rhtweedie12@gmail.com>
2022-05-21 23:21:12 +0100

    Fix typo

--- rptodo/config.py
+++ rptodo/config.py
@@ -8,7 +8,7 @@ from rptodo import (
     DB_WRITE_ERROR, DIR_ERROR, FILE_ERROR, SUCCESS, __app_name__
 )
 
-CONFIG_DIR_PATH = Path(typer.get.app_dir(__app_name__))
+CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
 CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
 
 def init_app(db_path: str) -> int:
